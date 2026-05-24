61aaa57d2d83703907cab9e52cfdd3a2275ad5f9
Yuvraj Singh <131570406+yuvrajsingh08@users.noreply.github.com>
2026-05-24 09:27:00 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (25.07%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxPos = nums[0];
+        int n = nums.size();
+        for(int i=1; i<n; i++) {
+            if(i <= maxPos) {
+                // update 
+                maxPos = max(maxPos, nums[i] + i);
+            } else return false;
+        }
+        return true;
+    }
+};
