dcad69582f6adf7bd442a7a9e29adfbdf6651d2f
Yuvraj Singh <131570406+yuvrajsingh08@users.noreply.github.com>
2026-07-09 22:31:39 +0530

    Time: 9 ms (76.16%) | Memory: 224.3 MB (60.27%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    unordered_map<int,int> parent;
-    int doMapping(int n, vector<int>& nums, int maxDiff, int curr) {
-        if(curr == n-1 || nums[curr+1] - nums[curr] > maxDiff){
-            parent[curr] = curr;
-            if(curr != n-1) {
-                doMapping(n,nums,maxDiff,curr+1);
-            }
-            return curr; 
-        } 
-        return parent[curr] = doMapping(n,nums,maxDiff, curr+1);
-    }
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        doMapping(n,nums,maxDiff,0);
-        vector<bool> ans;
-        for(auto c: queries) {
-            int u = c[0];
-            int v = c[1];
-            ans.push_back(parent[u] == parent[v]);
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                      vector<vector<int>>& queries) {
+
+        vector<int> comp(n);
+        int id = 0;
+        comp[0] = id;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff)
+                id++;
+            comp[i] = id;
         }
+
+        vector<bool> ans;
+        for (auto &q : queries) {
+            ans.push_back(comp[q[0]] == comp[q[1]]);
+        }
+
         return ans;
     }
 };
