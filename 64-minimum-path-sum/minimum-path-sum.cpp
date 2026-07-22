fc6e11bbe2e6eae70733e765a52c8312bd2abb26
Yuvraj Singh <131570406+yuvrajsingh08@users.noreply.github.com>
2026-07-22 21:38:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (42.93%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solveUsingRecursion(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp) {
+        if(i>=grid.size() || j>=grid[0].size()) return 10000000;
+        if(i == grid.size()-1 && j == grid[0].size()-1 ) return grid[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        long long down = grid[i][j]+ solveUsingRecursion(grid,i,j+1,dp);
+        long long up = grid[i][j]+ solveUsingRecursion(grid,i+1,j,dp);
+        // cout<<i<<" "<<j<<" "<<" up "<<up<<" down "<<down<<" "<<endl;
+        return dp[i][j] = min(up,down);
+    }
+    int solveUsingTab(vector<vector<int>>& grid) {
+        vector<vector<int>> dp(grid.size()+1,vector<int>(grid[0].size()+1,10000000));
+        dp[grid.size()-1][grid[0].size()-1] = grid[grid.size()-1][grid[0].size()-1];
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i=m-1; i>=0; i--) {
+            for(int j=n-1; j>=0; j--) {
+                if(i == m-1 && j == n-1) continue;
+                long long down = grid[i][j]+ dp[i][j+1];
+                long long up = grid[i][j]+ dp[i+1][j];
+                dp[i][j] = min(up,down);
+            }
+        }
+        return dp[0][0];
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int sum = 0;
+        // vector<vector<int>> dp(grid.size()+1,vector<int>(grid[0].size()+1,-1));
+        return solveUsingTab(grid);
+        // return solveUsingRecursion(grid, 0,0,dp);
+    }
+};
